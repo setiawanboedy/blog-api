619a5d017d7a493fb55f67bf5902db8e13955b1d
setiawanboedy <gonisetiawan0@gmail.com>
2024-09-09 15:42:32 +0800

    security problem solved remove scan

--- src/main/kotlin/blog/restful/tafakkur/com/BlogApiApplication.kt
+++ src/main/kotlin/blog/restful/tafakkur/com/BlogApiApplication.kt
@@ -1,12 +1,9 @@
 package blog.restful.tafakkur.com
 
-import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan(basePackages = ["blog.restful.tafakkur.com.repository"])
 class BlogApiApplication
 
 fun main(args: Array<String>) {
