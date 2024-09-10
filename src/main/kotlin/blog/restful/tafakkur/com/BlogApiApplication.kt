cadad05fae04ac499aad49b0109ade8ce70f05c5
setiawanboedy <gonisetiawan0@gmail.com>
2024-09-10 11:21:05 +0000

    build with compose

--- src/main/kotlin/blog/restful/tafakkur/com/BlogApiApplication.kt
+++ src/main/kotlin/blog/restful/tafakkur/com/BlogApiApplication.kt
@@ -6,15 +6,11 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-val logger = LoggerFactory.getLogger(BlogApiApplication::class.java)
-
 @SpringBootApplication
 @ComponentScan(basePackages = ["blog.restful.tafakkur.com.repository"])
 @EnableJpaRepositories(basePackages = ["blog.restful.tafakkur.com.repository"])
 class BlogApiApplication
 
 fun main(args: Array<String>) {
-	val port = System.getenv("PORT")
-	logger.info("Application is starting on port: $port")
 	runApplication<BlogApiApplication>(*args)
 }
